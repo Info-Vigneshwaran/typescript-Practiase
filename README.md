4f6808b64c9bf8a0804d787bc402b7e71019092d
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-04-21 17:16:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-au44dm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-au44dm)
---
+++ index.ts
@@ -0,0 +1,12 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+class Name {
+  name: string;
+  constructor() {
+    this.name = 12;
+  }
+}

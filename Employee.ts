110db48e461a3aef9569579a87212602860b4fbe
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-04-21 19:11:40 +0530

    1 file modified

--- Employee.ts
+++ Employee.ts
@@ -2,7 +2,7 @@ class Employee {
   id: number;
   name: string;
   age: number;
-  constructor(id, name, age) {
+  constructor(id: number, name: string, age: number) {
     this.id = id;
     this.name = name;
     this.age = age;
